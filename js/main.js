b8767d8554782289e784ef443deee31840ef1e3c
Patrick Sheehan <psheehan0301@gmail.com>
2015-03-19 17:45:23 -0500

    Got TweenMax to work for animations

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function tweenaway() {
+	var elem = document.getElementById("welcome")
+	TweenMax.to(elem, 2, {bottom:20})
+}
+
+// function dropWelcomeText() {
+
+// 	var elem = document.getElementById("welcome")
+// 	var vPos = 50
+
+// 	function frame() {
+// 		vPos += 2
+// 		elem.style.top = vPos + 'px'
+
+// 		var rect = elem.getBoundingClientRect()
+// 		var bottom = rect.bottom
+// 		var height = window.innerHeight
+
+// 		if (height - bottom < 20)
+// 			clearInterval(id)
+// 	}
+
+// 	var id = setInterval(frame, 0.25)
+// }
+
